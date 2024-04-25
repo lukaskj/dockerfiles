de00eea62f84fdc463eba61bc500a79c2f1e583b
Lukas Jacomin <lukasjacomin@gmail.com>
2024-04-25 19:43:43 -0300

    script

--- minecraft/scripts/getpaperserver.sh
+++ minecraft/scripts/getpaperserver.sh
@@ -1,5 +1,7 @@
 # original script by https://github.com/astorks/PaperMC.sh/blob/master/papermc.sh
 
+set -e
+
 PAPERMC_VERSION=$1
 LATEST_BUILD=$(curl -s "https://papermc.io/api/v2/projects/paper/versions/${PAPERMC_VERSION}" | jq '.builds[-1]')
 LATEST_DOWNLOAD=$(curl -s "https://papermc.io/api/v2/projects/paper/versions/${PAPERMC_VERSION}/builds/${LATEST_BUILD}" | jq '.downloads.application.name' -r)
